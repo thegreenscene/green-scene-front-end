f47799a35168303eaeec8d196968cd2cd4194b62
Trembloc <tremblay34487@gmail.com>
2025-03-03 16:18:08 -0500

    added a login form component

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,8 +1,20 @@
 
+
+//displays a Login form
 const Login = () => {
   return (
     <>
       <h1>LOGIN</h1>
+      <form> 
+        <label>
+          
+          Username: <input/>
+        </label>
+        <label>
+          Password: <input/>
+        </label>
+        <button>Log In</button>
+      </form>
     </>
   )
 
