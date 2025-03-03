0717e8b7ebefc0741698ed010a31b6797d1eecd5
Rythmat <teotaniwha@gmail.com>
2025-03-03 11:30:13 -0700

    add component skeletons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Home from "./Home.jsx"
+import Post from "./Post.jsx"
+import Register from "./Register.jsx"
+import Login from "./Login.jsx"
+
+import { Routes, Route } from 'react-router-dom'
+
 const App = () => {
 
   return (
     <>
       <h1>Welcome to the Green Scene </h1>
+
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        {/* ROUTES HERE */}
+      </Routes>
     </>
   )
 }
---
+++ src/Post.jsx
@@ -0,0 +1,11 @@
+
+const Post = () => {
+  return (
+    <>
+      <h1>POST</h1>
+    </>
+  )
+
+}
+
+export default Post;
