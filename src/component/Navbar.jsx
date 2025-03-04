2a5f4197d917a6cce89751855af411ab62503847
Trembloc <tremblay34487@gmail.com>
2025-03-04 15:14:22 -0500

    added basic post content, added basic css

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,11 +3,19 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
 
   return (
-    <div id="navbar">      
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/register">Register</Link>
-      <Link to="/post">Post</Link>
+    <div id="navbar">
+      <a>
+        <Link to="/">Home</Link>
+      </a>
+      <a>
+        <Link to="/login">Login</Link>
+      </a>
+      <a>
+        <Link to="/register">Register</Link>
+      </a>
+      <a>
+        <Link to="/post">Post</Link>
+      </a>
     </div>
   )
 }
--- src/component/Post.jsx
+++ src/component/Post.jsx
@@ -3,6 +3,41 @@ const Post = () => {
   return (
     <>
       <h1>POST</h1>
+      <form>
+        <label>
+          Product Name: <input />
+        </label>
+        <label>
+          Quantity: <input />
+        </label>
+        <label>
+          Location: <input />
+        </label>
+        <label>
+          Price: <input />
+        </label>
+        <label>
+          img_url: <input />
+        </label>
+        <label>
+          Description: <input />
+        </label>
+        <p>Type:</p>
+        <select>
+          {/* Future jack, make sure when you add event listeners to each option 
+          to have them return a strict string. It will obviously be different to just normal
+          user input*/}
+          <option>
+            Seed
+          </option>
+          <option>
+            Tool
+          </option>
+          <option>
+            Sprout
+          </option>
+        </select>
+      </form>
     </>
   )
 
