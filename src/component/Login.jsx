6b9d875726226a457661cac969d28a168f68eb5a
Daniel Almeida <almeida.danny@gmail.com>
2025-03-04 14:50:27 -0600

    add userlogin and register user components.

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,23 +1,44 @@
-
+import { useState } from "react";
 
 //displays a Login form
-const Login = () => {
+const Login = () => { 
+  const [inputUser, setInputUser] = useState('');
+  const [inputPass, setInputPass] = useState('');
+
+const loginAttempt = async (event) => {
+  event.preventDefault();
+
+  const responseAttempt = await fetch('https://green-scene.onrender.com/api/auth/login', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      username: inputUser,
+      password: inputPass
+    })
+  });
+  const tokenObject = await responseAttempt.json();
+  
+  if(tokenObject.token){
+    localStorage.setItem('token', tokenObject.token);
+  }
+}
+
   return (
     <>
       <h1>LOGIN</h1>
-      <form> 
+      <form onSubmit={loginAttempt}> 
         <label>
-          
-          Username: <input/>
+          Username: <input onChange={(event) => {setInputUser(event.target.value)}}/>
         </label>
         <label>
-          Password: <input/>
+          Password: <input type="password" onChange={(event) => {setInputPass(event.target.value)}}/>
         </label>
         <button>Log In</button>
       </form>
     </>
   )
-
 }
 
 export default Login;
